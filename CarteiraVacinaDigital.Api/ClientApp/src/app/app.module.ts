1a45bc51ed3eed91aa379366148ae843e4bd722c
Elton Pereira <eltonpereira94@hotmail.com>
2021-05-23 22:03:06 -0300

    Separar componentes de layout dos componentes dos módulos

--- CarteiraVacinaDigital.Api/ClientApp/src/app/app.module.ts
+++ CarteiraVacinaDigital.Api/ClientApp/src/app/app.module.ts
@@ -1,32 +1,30 @@
+import { FuncionarioComponent } from './module/funcionario/funcionario.component';
+import { HomeComponent } from './home/home.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
-import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
-import { NavMenuComponent } from './nav-menu/nav-menu.component';
-import { HomeComponent } from './home/home.component';
-import { CounterComponent } from './counter/counter.component';
-import { FetchDataComponent } from './fetch-data/fetch-data.component';
+import { HeaderComponent } from './layout/header/header.component';
+import { FooterComponent } from './layout/footer/footer.component';
+import { LoginComponent } from './core/login/login.component';
+import { AppRoutingModule } from './/app-routing.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavMenuComponent,
+    HeaderComponent,
+    FooterComponent,
+    LoginComponent,
     HomeComponent,
-    CounterComponent,
-    FetchDataComponent
+    FuncionarioComponent
   ],
   imports: [
     BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
     HttpClientModule,
     FormsModule,
-    RouterModule.forRoot([
-      { path: '', component: HomeComponent, pathMatch: 'full' },
-      { path: 'counter', component: CounterComponent },
-      { path: 'fetch-data', component: FetchDataComponent },
-    ])
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
