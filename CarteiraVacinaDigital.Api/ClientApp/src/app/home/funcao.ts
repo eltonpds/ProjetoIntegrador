80a2a1d2313993835d46f8a69b9571d70dbec857
Elton Pereira <eltonpereira94@hotmail.com>
2021-05-23 22:03:57 -0300

    Elaborar componente principal com botãos na tela inicial para cada módulo

---
+++ CarteiraVacinaDigital.Api/ClientApp/src/app/home/funcao.ts
@@ -0,0 +1,5 @@
+export class Funcao {
+  id: number;
+  name: string;
+  url: string;
+}
--- CarteiraVacinaDigital.Api/ClientApp/src/app/home/home.component.ts
+++ CarteiraVacinaDigital.Api/ClientApp/src/app/home/home.component.ts
@@ -1,8 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { Funcao } from './funcao';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+
+  ngOnInit() {
+
+  }
+
+  funcao: Funcao[] = [
+    { id: 1, name: 'Funcionário', url: 'funcionario' },
+    { id: 2, name: 'Agendamento', url: 'agendamento' },
+    { id: 3, name: 'Histórico', url: 'historico' },
+    { id: 4, name: 'Registrar Vacina', url: 'registrarvacina' }
+  ];
 }
