f6df4f12a95ab67bfaa2a395adb5fe93302835f2
Elton Pereira <eltonpereira94@hotmail.com>
2021-05-26 20:48:33 -0300

    Criar classe Pacient e sua ViewModel

---
+++ CarteiraVacinaDigital.Model/Entities/ViewModel/PacientViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CarteiraVacinaDigital.Model.Entities.ViewModel
+{
+    public class PacientViewModel
+    {
+        public Pacient  Pacient { get; set; }
+        public int ScheduleId { get; set; }
+    }
+}
