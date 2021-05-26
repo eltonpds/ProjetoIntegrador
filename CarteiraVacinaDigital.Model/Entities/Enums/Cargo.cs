552c66d8ea6be1b51d6d2ad2a226f626312f41cd
Elton Pereira <eltonpereira94@hotmail.com>
2021-05-26 20:50:00 -0300

    Criar classe Vaccine

---
+++ CarteiraVacinaDigital.Model/Entities/Enums/Cargo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CarteiraVacinaDigital.Model.Entities.Enums
+{
+    public enum Cargo : int
+    {
+        Administrador,
+        Tecnico,
+        Enfermeiro,
+        Medico,
+        Aprendiz,
+        Estagiario
+    }
+}
---
+++ CarteiraVacinaDigital.Model/Entities/Vaccine.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CarteiraVacinaDigital.Model.Entities
+{
+    public class Vaccine
+    {
+        public int Id { get; set; }
+        public string FactoryName { get; set; }
+        public string VaccineName { get; set; }
+        public DateTime ManufacturingDate { get; set; }
+        public DateTime Validate { get; set; }
+        public string Lote { get; set; }
+    }
+}
