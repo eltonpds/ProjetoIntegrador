88c3f1408698073ea557fda7ef76e376466ad648
Elton Pereira <eltonpereira94@hotmail.com>
2021-05-26 20:54:08 -0300

    Criar classe Campaign

---
+++ CarteiraVacinaDigital.Model/Entities/Campaign.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CarteiraVacinaDigital.Model.Entities
+{
+    public class Campaign
+    {
+        public int Id { get; set; }
+        public string CampaignName { get; set; }
+        public string Information { get; set; }
+        public ICollection<Vaccine> Vaccine { get; set; }
+        public int VaccineId { get; set; }
+    }
+}
