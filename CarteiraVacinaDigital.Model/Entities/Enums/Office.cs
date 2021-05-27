06c256ea854a51c655e45c3bd6f6166a6ecd2ec9
Elton Pereira <eltonpereira94@hotmail.com>
2021-05-27 14:44:30 -0300

    Refatoração da classe Enum Office

--- CarteiraVacinaDigital.Model/Entities/Enums/Office.cs
+++ CarteiraVacinaDigital.Model/Entities/Enums/Office.cs
@@ -4,7 +4,7 @@ using System.Text;
 
 namespace CarteiraVacinaDigital.Model.Entities.Enums
 {
-    public enum Cargo : int
+    public enum Office : int
     {
         Administrador,
         Tecnico,
