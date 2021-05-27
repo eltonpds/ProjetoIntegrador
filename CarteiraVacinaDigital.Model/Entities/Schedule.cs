74d9478b12e01642fb9d169dec767563846e854d
Elton Pereira <eltonpereira94@hotmail.com>
2021-05-26 21:02:38 -0300

    Criar classe Schedule com sua ViewModel

---
+++ CarteiraVacinaDigital.Model/Entities/Schedule.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CarteiraVacinaDigital.Model.Entities
+{
+    public class Schedule
+    {
+        public int Id { get; set; }
+        public DateTime DateSchedule { get; set; }
+        public ICollection<Pacient> Pacients { get; set; }
+        public int PacientId { get; set; }
+        public ICollection<Vaccine> Vaccines { get; set; }
+        public int VaccineId { get; set; }
+    }
+}
