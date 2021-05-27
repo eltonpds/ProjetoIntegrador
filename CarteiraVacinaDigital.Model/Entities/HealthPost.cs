5dde63cfb845f5157cb857a935ae39a0b106c7a7
Elton Pereira <eltonpereira94@hotmail.com>
2021-05-26 21:04:12 -0300

    Criar classe HealthPost e sua ViewModel

---
+++ CarteiraVacinaDigital.Model/Entities/HealthPost.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CarteiraVacinaDigital.Model.Entities
+{
+    public class HealthPost
+    {
+        public int Id { get; set; }
+        public string HealthUnit { get; set; }
+        public string SanitaryDistrict { get; set; }
+        public string Address { get; set; }
+        public ICollection<Vaccine> Vaccines { get; set; }
+    }
+}
---
+++ CarteiraVacinaDigital.Model/Entities/ViewModel/HealthPostViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CarteiraVacinaDigital.Model.Entities.ViewModel
+{
+    public class HealthPostViewModel
+    {
+        public HealthPost HealthPost { get; set; }
+        public Vaccine Vaccine { get; set; }
+    }
+}
